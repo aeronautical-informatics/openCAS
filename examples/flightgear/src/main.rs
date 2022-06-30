f85c5b0073eda2ea762a4f3ee6a129a73363b3a5
autumnal <friedrich122112@me.com>
2022-06-30 10:29:58 +0200

    Initial Example commit

---
+++ examples/flightgear/src/main.rs
@@ -0,0 +1,133 @@
+//! This demo illustrates how the openCAS system can be integrated with Flightgear
+
+use std::{env, error::Error, time::Instant};
+
+use futures::prelude::*;
+
+use async_tungstenite::tungstenite::Message;
+use opencas::*;
+use serde::{Deserialize, Serialize};
+use uom::si::angle::degree;
+use uom::si::f32::*;
+use uom::si::time::second;
+use uom::si::velocity::knot;
+
+#[derive(Serialize)]
+struct FlightgearCommand {
+    command: String,
+    node: String,
+}
+
+/// Yields AircraftStates from a Flightgear http/json connection
+///
+/// # Arguments
+/// `base_uri` - The base URI of the Flightgear http interface. Something like `localhost:5400`.
+async fn new_flightgear_stream(
+    base_uri: &str,
+) -> Result<impl Stream<Item = Result<PropertyTreeLeaf, Box<dyn Error>>>, Box<dyn Error>> {
+    let url = format!("ws://{}/PropertyListener", base_uri);
+    let (mut stream, _) = async_tungstenite::async_std::connect_async(url).await?;
+
+    for node in KEYS {
+        let sub = FlightgearCommand {
+            command: "addListener".to_string(),
+            node: node.to_string(),
+        };
+        stream
+            .send(Message::Binary(serde_json::to_vec(&sub)?))
+            .await?;
+    }
+
+    Ok(stream
+        .map(|msg| -> Result<_, Box<dyn Error>> { Ok(serde_json::from_slice(&msg?.into_data())?) }))
+}
+
+#[derive(Deserialize)]
+struct PropertyTreeLeaf {
+    pub path: String,
+    pub ts: f32,
+    pub value: f32,
+}
+
+#[derive(Clone, Default)]
+struct AircraftState {
+    pub groundspeed: Velocity,
+    pub lng: Angle,
+    pub lat: Angle,
+    pub heading: Angle,
+}
+
+const KEYS: &[&str] = &[
+    "/velocities/groundspeed-kt",
+    "/position/longitude-deg",
+    "/position/latitude-deg",
+    "/orientation/heading-deg",
+    "/ai/models/aircraft[0]/velocities/groundspeed-kt",
+    "/ai/models/aircraft[0]/position/longitude-deg",
+    "/ai/models/aircraft[0]/position/latitude-deg",
+    "/ai/models/aircraft[0]/orientation/heading-deg",
+];
+
+const USAGE: &str = "usage: <Flightgear base url>";
+
+// http://localhost:5400/json/velocities?i=y&t=y&d=3
+
+fn main() -> Result<(), Box<dyn Error>> {
+    smol::block_on(async {
+        let args: Vec<String> = env::args().collect();
+        let base_uri = args.get(1).expect(USAGE);
+
+        let mut fg_stream = new_flightgear_stream(base_uri.as_str()).await?;
+
+        let mut hcas = HCas {
+            last_advisory: HAdvisory::ClearOfConflict,
+        };
+        let mut ai = AircraftState::default();
+        let mut user = AircraftState::default();
+
+        let mut ts = Time::new::<second>(0.0);
+
+        loop {
+            let leaf = fg_stream.next().await.unwrap()?;
+            let now = Instant::now();
+            let cur_ts = Time::new::<second>(leaf.ts);
+
+            // Next frame begins
+            if cur_ts > ts {
+                let psi = user.heading - ai.heading;
+                let forward_range = todo!();
+                let left_range = todo!();
+                let tau = todo!();
+
+                let (adv, _) = hcas.process_cartesian(tau, forward_range, left_range, psi);
+                hcas.last_advisory = adv;
+
+                println!("Processed frame, time consumed: {:?}", now.elapsed(),);
+                println!("{:?}", adv);
+                ts = cur_ts;
+            }
+
+            match leaf.path.as_str() {
+                "/velocities/groundspeed-kt" => {
+                    user.groundspeed = Velocity::new::<knot>(leaf.value)
+                }
+                "/position/longitude-deg" => user.lng = Angle::new::<degree>(leaf.value),
+                "/position/latitude-deg" => user.lat = Angle::new::<degree>(leaf.value),
+                "/orientation/heading-deg" => user.heading = Angle::new::<degree>(leaf.value),
+                "/ai/models/aircraft[0]/velocities/groundspeed-kt" => {
+                    ai.groundspeed = Velocity::new::<knot>(leaf.value)
+                }
+                "/ai/models/aircraft[0]/position/longitude-deg" => {
+                    ai.lng = Angle::new::<degree>(leaf.value)
+                }
+                "/ai/models/aircraft[0]/position/latitude-deg" => {
+                    ai.lat = Angle::new::<degree>(leaf.value)
+                }
+                "/ai/models/aircraft[0]/orientation/heading-deg" => {
+                    ai.heading = Angle::new::<degree>(leaf.value)
+                }
+                _ => {}
+            }
+        }
+    })
+}
