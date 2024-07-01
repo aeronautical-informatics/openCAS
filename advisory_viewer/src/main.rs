c1a95a5fa84db9c372a770c94456725f16798976
Wanja Zaeske <wanja.zaeske@dlr.de>
2024-07-01 15:59:50 +0200

    fix: branding

--- advisory_viewer/src/main.rs
+++ advisory_viewer/src/main.rs
@@ -8,7 +8,7 @@ fn main() -> eframe::Result<()> {
 
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
-        "eframe template",
+        "openCAS Advisory Viewer",
         native_options,
         Box::new(|cc| Box::new(advisory_viewer::TemplateApp::new(cc))),
     )
